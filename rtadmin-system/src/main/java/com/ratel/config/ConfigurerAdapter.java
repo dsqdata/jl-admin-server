d67759c9b8f39e8fc9186c09068fe74a86ac7925
dongchuang <yy6yy@live.cn>
2020-03-04 18:27:40 +0800

    初始化

--- rtadmin-system/src/main/java/com/ratel/config/ConfigurerAdapter.java
+++ rtadmin-system/src/main/java/com/ratel/config/ConfigurerAdapter.java
@@ -26,6 +26,9 @@ public class ConfigurerAdapter implements WebMvcConfigurer {
     @Value("${file.html}")
     private String html;
 
+    @Value("${file.jarHtml}")
+    private String jarHtml;
+
     @Bean
     public CorsFilter corsFilter() {
         UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
@@ -43,9 +46,14 @@ public class ConfigurerAdapter implements WebMvcConfigurer {
         String avatarUtl = "file:" + avatar.replace("\\","/");
         String pathUtl = "file:" + path.replace("\\","/");
         String htmlUtl = "file:" + html.replace("\\","/");
+        String jarHtmlUtl = jarHtml.replace("\\","/");
         registry.addResourceHandler("/avatar/**").addResourceLocations(avatarUtl).setCachePeriod(0);
         registry.addResourceHandler("/file/**").addResourceLocations(pathUtl).setCachePeriod(0);
-        registry.addResourceHandler("/**").addResourceLocations(htmlUtl).setCachePeriod(0);
+        if(!"none".equals(jarHtml)){
+            registry.addResourceHandler("/**").addResourceLocations(jarHtmlUtl).setCachePeriod(0);
+        }else{
+            registry.addResourceHandler("/**").addResourceLocations(htmlUtl).setCachePeriod(0);
+        }
         registry.addResourceHandler("/statics/**").addResourceLocations("classpath:/statics/");
         // 解决 SWAGGER 404报错
         registry.addResourceHandler("/swagger-ui.html").addResourceLocations("classpath:/META-INF/resources/");
