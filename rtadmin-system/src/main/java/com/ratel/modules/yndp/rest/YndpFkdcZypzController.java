6dc7e73bfa53fb40a9d321e9f01eeaefdeb16ca5
xjl <598412644@qq.com>
2020-03-05 16:59:11 +0800

    样式提交

--- rtadmin-system/src/main/java/com/ratel/modules/yndp/rest/YndpFkdcZypzController.java
+++ rtadmin-system/src/main/java/com/ratel/modules/yndp/rest/YndpFkdcZypzController.java
@@ -9,8 +9,8 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 /**
  * @author DDXS
