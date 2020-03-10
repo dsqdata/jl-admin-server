96c8f940e31f542d6a23a56b72d57e23192e17c9
xjl <598412644@qq.com>
2020-03-10 18:56:33 +0800

    xjl提交

--- rtadmin-run/src/main/resources/html/src/js/threeCommon.js
+++ rtadmin-run/src/main/resources/html/src/js/threeCommon.js
@@ -11,11 +11,18 @@ $(function () {
         autoclose:true,//选中关闭
         todayBtn: true,//今日按钮
         language:'zh-CN',
-        defaultDate: "1990-1-1",
-       // initialDate:new Date(Date.parse(beforeDate.replace(/-/g,"/"))),
+        initialDate:new Date(),
+        // initialDate:new Date(Date.parse(beforeDate.replace(/-/g,"/"))),
         endDate:new Date(Date.parse(beforeDate.replace(/-/g,"/")))
         // endDate:"-1d" //小于当天的日期的设置  endDate: new Date() 今天的日期和以前的能选择
     }).on('changeDate',function(ev){
         selectTime = $("#datetimepicker").find("input").val();
+        check(selectTime.replace(/-/g,""))
     });
+    $('#datetimepicker').datetimepicker('setDate', new Date(Date.parse(beforeDate.replace(/-/g,"/")))); //把要设置的默认值放入new Date()里面
+
+
+    function check(date){
+
+    }
 });
